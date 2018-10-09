2554ad8828a0c270194cde6db05ece4b85a60e8b
Benoit Duval <benoit.duval.pro@gmail.com>
2018-10-09 10:54:59 +0200

    add counters

--- module/Application/src/Controller/StatsController.php
+++ module/Application/src/Controller/StatsController.php
@@ -21,7 +21,28 @@ class StatsController extends AbstractController
 		    $baseUrl    = $config['baseUrl'];
 
 		    $games = $this->gameTable->fetchAll(['eventId' => $eventId, 'userId' => $userId]);
+		    $stats = $this->statsTable->fetchAll(['eventId' => $eventId, 'userId' => $userId]);
 		    $user  = $this->userTable->fetchOne(['id' => $userId]);
+
+		    $counters = [
+		    	\Application\Model\Stats::POINT_SERVE  => 0,
+		    	\Application\Model\Stats::FAULT_SERVE  => 0,
+		    	\Application\Model\Stats::POINT_ATTACK => 0,
+		    	\Application\Model\Stats::FAULT_ATTACK => 0,
+		    	\Application\Model\Stats::POINT_BLOCK  => 0,
+		    ];
+		    foreach ($stats as $key => $stat) {
+		    	if ($stat->pointFor == \Application\Model\Stats::POINT_US ||
+		    		($stat->pointFor == \Application\Model\Stats::POINT_THEM &&
+		    		 in_array($stat->reason, [
+		    		 	\Application\Model\Stats::FAULT_SERVE ,
+		    		 	\Application\Model\Stats::FAULT_ATTACK,
+		    	     ]) 
+		    		)
+		    	) {
+		    		$counters[$stat->reason] ++;
+		    	}
+		    }
 
 	        foreach ($games as $key => $game) {
 	            $quality[$game->type][] = (int) $game->quality; 
@@ -36,6 +57,7 @@ class StatsController extends AbstractController
 		    	'quality' => $quality,
 		    	'average' => $average,
 		    	'user' => $user,
+		    	'counters' => $counters,
 		    ]);
 		    $view->setTerminal(true);
 		    
